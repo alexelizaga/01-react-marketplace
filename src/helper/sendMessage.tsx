4ac5f4c34673da0af419ef0a11be2e0d7c254bc0
alexelizaga <alexelizaga@icloud.com>
2022-05-11 10:59:31 +0200

    Web Events

--- src/helper/sendMessage.tsx
+++ src/helper/sendMessage.tsx
@@ -2,7 +2,7 @@ declare const window: any;
 
 export const sendMessage = (message: {} , url = '*') => {
   console.log(message);
-  if(window.parent) window.parent.postMessage(JSON.stringify(message), url);
+  if(window.parent) window.parent.postMessage(message, url);
   if(window.AndroidApp) window.AndroidApp.postMessage(JSON.stringify(message));
   if(window.webkit) window.webkit.messageHandlers.IOSApp.postMessage(JSON.stringify(message));
 };
